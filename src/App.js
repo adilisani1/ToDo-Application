ff79098e367232ecf5035511dc76b5606f9d3d55
Adil Isani <adil.isani1@gmail.com>
2022-05-16 10:59:56 +0500

    Initail Setup For Todo App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [todos, setTodo] = useState([
+    {
+
+      id: 1,
+      title: 'Finish React Series',
+      isComplete: false,
+
+    },
+    {
+
+      id: 2,
+      title: 'Go Grocery',
+      isComplete: true,
+
+    },
+    {
+
+      id: 3,
+      title: 'Take over world',
+      isComplete: false,
+
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container mt-5">
+      <div className="col-12 col-lg-6 align-items-center justify-content-center">
+        <h2>Todo App</h2>
+        <form onSubmit="#">
+          <input type="text" className="form-control" placeholder="What do you need to do?" />
+        </form>
+        <table class="table">
+          <tbody>
+
+            {todos.map(todo => (
+              <tr>
+                <td>
+                  {todo.title}
+                </td>
+                <td><button type="button" class="btn-close btn-sm" aria-label="Close"></button></td>
+              </tr>
+            ))}
+
+
+
+
+
+
+
+
+          </tbody>
+        </table>
+      </div>
+
+    </div >
   );
 }
 
